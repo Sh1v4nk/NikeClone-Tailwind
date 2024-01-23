c1d3dfa6d7f0f11249fffc9c0c14f0b60711763c
Sh1v4nk <shivankpandey113@gmail.com>
2024-01-23 11:49:22 +0530

    Add initial code and required assets

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'palanquin' : ['Palanquin', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
