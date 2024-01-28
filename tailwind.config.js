972e5c391cb39b893a53cc43d9100e82ff1e5a19
Sh1v4nk <shivankpandey113@gmail.com>
2024-01-29 00:21:59 +0530

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.html'],
+  content: ["*.html"],
   theme: {
     extend: {
+      colors: {
+        "pale-blue": "#AEC6CF",
+      },
       fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif'],
-        'palanquin' : ['Palanquin', 'sans-serif'],
+        "montserrat": ["Montserrat", "sans-serif"],
+        "palanquin": ["Palanquin", "sans-serif"],
       },
       backgroundImage: {
-        'collection': "url('../Assets/images/collection-background.svg')",
-        'thumbnail': "url('../Assets/images/thumbnail-background.svg')",
-      }
+        "collection": "url('../Assets/images/collection-background.svg')",
+        "thumbnail": "url('../Assets/images/thumbnail-background.svg')",
+      },
     },
   },
   plugins: [],
-}
+};
